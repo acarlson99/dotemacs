920d5b8ed44ff691f96222c40626a8dbd3b6369f
unknown <bombblob@gmail.com>
2019-02-14 10:09:00 -0800

    Fixed some things in my-c++-config and made insert-exception ignore empty nested classes

--- lisp/my-c++-config.el
+++ lisp/my-c++-config.el
@@ -1,37 +1,40 @@
 ;Coplien form for c++ coding ease
-(defun hpp (name)
+(defun c++-insert-class-header (name)
     "Add Coplien form to .hpp for C++ Classes painlessly"
     (interactive "sClass name: ")
     (insert "\n#ifndef "(upcase name)"_HPP\n# define "(upcase name)"_HPP\n")
 	(insert "\n")
-    (insert "# include <iostream>\n# include <string>\n# include <stdexcept>\n\n")
+    ;; (insert "# include <iostream>\n# include <string>\n# include <stdexcept>\n\n")
     (insert "class "name" {\n\npublic:\n")
-    (insert "\t"name"( std::string );\n")
+    ;; (insert "\t"name"( std::string );\n")
     (insert "\t"name"( void );\n")
     (insert "\t"name"( "name" const & cp);\n")
     (insert "\t~"name"( void );\n")
     (insert "\t"name"& operator=( "name" const &);\n")
     (insert "\nprivate:\n\n};\n\n#endif\n")
 )
-(defun cpp (name)
+
+(defun c++-insert-class-src (name)
     "Add Coplien form to .cpp for C++ Classes painlessly"
     (interactive "sClass name: ")
     (insert "\n#include \""name".hpp\"\n\n")
-    (insert name"::"name"( std::string ) : { }\n")
+    ;; (insert name"::"name"( std::string ) : { }\n")
     (insert name"::"name"( void ) { }\n")
     (insert name"::"name"( "name" const & cp) { *this = cp; }\n")
     (insert name"::~"name"( void ) { }\n")
     (insert name"& "name"::operator=( "name" const &) { return *this; }\n")
 )
-(defun class-insert (name)
+
+(defun c++-insert-class (name)
     "Coplien Form Caller"
-    (interactive "s: ")
+    (interactive "sClass name: ")
     (if (equal (file-name-extension(buffer-file-name)) "hpp")
-        (hpp name))
+        (c++-insert-class-header name))
     (if (equal (file-name-extension(buffer-file-name)) "cpp")
-        (cpp name))
+        (c++-insert-class-src name))
 )
-(defun hppexc (name nest)
+
+(defun c++-insert-exception-header (name nest)
     "Add Coplien form to .hpp for C++ Exceptions painlessly"
     (interactive "sClass name: \nsNested in: ")
     (insert "\tclass "name"Exception : public std::exception {\n\tpublic:\n")
@@ -42,29 +45,40 @@
     (insert "\t\tvirtual const char* what() const throw();\n")
     (insert "\t};\n")
 )
-(defun cppexc (name nest)
-    "Add Coplien form to .cpp for C++ Exceptions painlessly"
-    (interactive "sClass name: \nsNested in: ")
-    (insert nest"::"name"Exception::"name"Exception( void ) { }\n")
-    (insert nest"::"name"Exception::"name"Exception( "name"Exception const & cp) { *this = cp; }\n")
-    (insert nest"::"name"Exception::~"name"Exception( void ) throw() { }\n")
-    (insert nest"::"name"Exception& "nest"::"name"Exception::operator=( "name"Exception const &) { return *this; }\n")
-    (insert "const char* "nest"::"name"Exception::what( void ) const throw() {\n\treturn \"ERR_MSG\";\n}\n")
-)
-(defun exception-insert (name nest)
+
+(defun c++-insert-exception-src (name nest)
+  "Add Coplien form to .cpp for C++ Exceptions painlessly"
+  (interactive "sClass name: \nsNested in: ")
+  (if (string= nest "")
+	  (progn
+		(insert name"Exception::"name"Exception( void ) { }\n")
+		(insert name"Exception::"name"Exception( "name"Exception const & cp) { *this = cp; }\n")
+		(insert name"Exception::~"name"Exception( void ) throw() { }\n")
+		(insert name"Exception& "name"Exception::operator=( "name"Exception const &) { return *this; }\n")
+		(insert "const char* "name"Exception::what( void ) const throw() {\n\treturn \"ERR_MSG\";\n}\n")
+		)
+	(progn
+	  (insert nest"::"name"Exception::"name"Exception( void ) { }\n")
+      (insert nest"::"name"Exception::"name"Exception( "name"Exception const & cp) { *this = cp; }\n")
+      (insert nest"::"name"Exception::~"name"Exception( void ) throw() { }\n")
+      (insert nest"::"name"Exception& "nest"::"name"Exception::operator=( "name"Exception const &) { return *this; }\n")
+      (insert "const char* "nest"::"name"Exception::what( void ) const throw() {\n\treturn \"ERR_MSG\";\n}\n")
+	  )))
+
+(defun c++-insert-exception (name nest)
     "Exception Coplien Form Caller"
     (interactive "sException Name: \nsNested in class: ")
     (if (equal (file-name-extension(buffer-file-name)) "hpp")
-        (hppexc name nest))
+        (c++-insert-exception-header name nest))
     (if (equal (file-name-extension(buffer-file-name)) "cpp")
-        (cppexc name nest))
+        (c++-insert-exception-src name nest))
 )
 
 (defun my-c++-config()
   "For use in 'c++-mode-hook'."
   ;; template exception insertion stuff from Steven
-  (local-set-key (kbd "C-c t i") 'class-insert)
-  (local-set-key (kbd "C-c t e") 'exception-insert)
+  (local-set-key (kbd "C-c i i") 'c++-insert-class)
+  (local-set-key (kbd "C-c i e") 'c++-insert-exception)
   ;; better comments
   (local-set-key (kbd "C-c C-c") 'comment-norminette)
   ;; whitespace cleanup
