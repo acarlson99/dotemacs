0b618bbe27676b17eddf9ede9acde15fdf43dedc
Alexander Carlson <acarlson@e1z2r3p23.42.us.org>
2019-03-25 14:39:55 -0700

    Fixed

--- lisp/my-web-config.el
+++ lisp/my-web-config.el
@@ -1,4 +1,3 @@
 (defun my-web-config()
   "For use in 'web-mode-hook'."
-  (web-mode-toggle-current-element-highlight)	;; TODO: this is bad.  Fix so it just sets it rather than toggle
-  )
+  (setq web-mode-enable-current-element-highlight t))
