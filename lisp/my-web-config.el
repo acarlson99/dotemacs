65a8e8d7a45b4305f74b060693ee5ab51ed5402f
Alexander Carlson <acarlson@e1z2r3p23.42.us.org>
2019-03-25 15:16:33 -0700

    Added commenting

--- lisp/my-web-config.el
+++ lisp/my-web-config.el
@@ -1,3 +1,7 @@
 (defun my-web-config()
   "For use in 'web-mode-hook'."
+  ;; commenting
+  (local-set-key (kbd "C-c C-c") 'comment-region)
+  ;; uncommenting
+  (local-set-key (kbd "C-c c") 'uncomment-region)
   (setq web-mode-enable-current-element-highlight t))
