d37f3030b03fd244b054ee3bf303977e191955da
John <bombblob@gmail.com>
2019-05-04 00:56:25 -0700

    Fixed silly function definition

--- lisp/my-go-config.el
+++ lisp/my-go-config.el
@@ -1,22 +1,21 @@
 (defun go-updade-and-save ()
   "Run goreturns to update buffer, includes, etc and save result."
   (interactive)
-  (save-buffer)
-  (point-to-register 1)
-  (erase-buffer)
-  (if (shell-command (concat "~/go/bin/goreturns " buffer-file-name) 1)
-	  (progn ;; success
-		(jump-to-register 1)
+  (if (and (file-exists-p "~/go/bin/goreturns") (file-executable-p "~/go/bin/goreturns"))
+	  (progn
 		(save-buffer)
-		)
-	(error "ERROR: Save failed.  Panic")) ;; failure
-  )
+		(erase-buffer)
+		(if (shell-command (concat "~/go/bin/goreturns " buffer-file-name) 1)
+			(save-buffer) ;; success
+		  (error "ERROR: Save failed.  Panic"))) ;; failure
+	(error "~/go/bin/goreturns does not exist or does not have execute permissions.  Run 'go get -v github.com/sqs/goreturns' to install")))
 
 (defun my-go-config ()
   "For use in 'go-mode-hook'."
-  (if (and (file-exists-p "~/go/bin/goreturns") (file-executable-p "~/go/bin/goreturns"))
-	  (local-set-key (kbd "C-x C-a") 'go-updade-and-save)
-	(local-set-key (kbd "C-x C-a") (lambda () (interactive) (message "~/go/bin/goreturns does not exist or does not have execute permissions"))))
+  (local-set-key (kbd "C-x C-a") 'go-updade-and-save)
+  ;; (if (and (file-exists-p "~/go/bin/goreturns") (file-executable-p "~/go/bin/goreturns"))
+  ;; 	  (local-set-key (kbd "C-x C-a") 'go-updade-and-save)
+  ;; 	(local-set-key (kbd "C-x C-a") (lambda () (interactive) (message "~/go/bin/goreturns does not exist or does not have execute permissions.  Run 'go get -v github.com/sqs/goreturns' to unstall"))))
   ;; whitespace cleanup
   (local-set-key (kbd "C-c w") 'whitespace-cleanup)
   ;; commenting
