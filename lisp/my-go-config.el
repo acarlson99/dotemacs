a7a7bd9680defe2265369692ea8351af797dc646
Alexander <alexandercarlson99@gmail.com>
2019-05-23 17:29:55 -0700

    Fixed indentation

--- lisp/my-go-config.el
+++ lisp/my-go-config.el
@@ -14,12 +14,13 @@
 (defun go-errcatch ()
   "Insert go error catch."
   (interactive)
-  (insert "if err != nil {\n")
+  (insert "if err != nil {")
   (indent-for-tab-command)
-  (insert "panic(1)\n")
+  (insert "\npanic(1) // TODO: address error")
   (indent-for-tab-command)
-  (insert "}\n")
-  (indent-for-tab-command))
+  (insert "\n}")
+  (indent-for-tab-command)
+  (insert "\n"))
 
 (defun my-go-config ()
   "For use in 'go-mode-hook'."
@@ -28,7 +29,7 @@
   ;; 	  (local-set-key (kbd "C-x C-a") 'go-updade-and-save)
   ;; 	(local-set-key (kbd "C-x C-a") (lambda () (interactive) (message "~/go/bin/goreturns does not exist or does not have execute permissions.  Run 'go get -v github.com/sqs/goreturns' to unstall"))))
   ;; go error check
-  (local-set-key (kbd "C-c i e") 'go-errcatch)
+  (local-set-key (kbd "C-c C-e") 'go-errcatch)
   ;; whitespace cleanup
   (local-set-key (kbd "C-c w") 'whitespace-cleanup)
   ;; commenting
