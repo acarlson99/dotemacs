5138f017cc9d0f6a748ec3d34b6319830a0110be
Alexander <alexandercarlson99@gmail.com>
2019-05-23 17:23:39 -0700

    Added go error catching

--- lisp/my-go-config.el
+++ lisp/my-go-config.el
@@ -10,7 +10,16 @@
 		  (error "ERROR: Save failed.  Panic"))) ;; failure
 	(if (y-or-n-p "~/go/bin/goreturns does not exist or does not have execute permissions.  Run 'go get -v github.com/sqs/goreturns' to install?")
 		(shell-command "go get -v github.com/sqs/goreturns"))))
-	;; (error "~/go/bin/goreturns does not exist or does not have execute permissions.  Run 'go get -v github.com/sqs/goreturns' to install")))
+
+(defun go-errcatch ()
+  "Insert go error catch."
+  (interactive)
+  (insert "if err != nil {\n")
+  (indent-for-tab-command)
+  (insert "panic(1)\n")
+  (indent-for-tab-command)
+  (insert "}\n")
+  (indent-for-tab-command))
 
 (defun my-go-config ()
   "For use in 'go-mode-hook'."
@@ -18,6 +27,8 @@
   ;; (if (and (file-exists-p "~/go/bin/goreturns") (file-executable-p "~/go/bin/goreturns"))
   ;; 	  (local-set-key (kbd "C-x C-a") 'go-updade-and-save)
   ;; 	(local-set-key (kbd "C-x C-a") (lambda () (interactive) (message "~/go/bin/goreturns does not exist or does not have execute permissions.  Run 'go get -v github.com/sqs/goreturns' to unstall"))))
+  ;; go error check
+  (local-set-key (kbd "C-c i e") 'go-errcatch)
   ;; whitespace cleanup
   (local-set-key (kbd "C-c w") 'whitespace-cleanup)
   ;; commenting
