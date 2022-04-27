38c17e36de3aa25a69ee448e9caaf894d0e3ed61
Alexander Carlson <alexanderjc@google.com>
2022-04-27 17:53:04 +0000

    prune weekly

--- lisp/prune-backups.el
+++ lisp/prune-backups.el
@@ -10,15 +10,25 @@
 (setq auto-save-file-name-transforms
 	  `((".*" , "~/.emacs.d/backups" t)))
 
-;; Purges files not accessed in a week
-(message "Deleting old backup files...")
-(let ((week (* 60 60 24 7))
-	  (current (float-time (current-time))))
-  (dolist (file (directory-files "~/.emacs.d/backups" t))
-	(when (and (backup-file-name-p file)
-			   (> (- current (float-time (nth 5 (file-attributes file))))
-				  week))
-	  (message "%s" file)
-	  (delete-file file))))
+(let ((last-run-time-file "~/.emacs.d/.lastIwiped")
+	  (week (* 60 60 24 7)))
+  (if (not (file-exists-p last-run-time-file))
+	  (write-region (number-to-string (float-time (current-time))) nil last-run-time-file))
+  (let ((time (with-temp-buffer
+				(insert-file-contents last-run-time-file)
+				(string-to-number (buffer-string))))
+		(current (float-time (current-time))))
+	(if (> current (+ time week))
+		(progn
+		  ;; Purges files not accessed in a week
+		  (message "Deleting old backup files...")
+		  (dolist (file (directory-files "~/.emacs.d/backups" t))
+			(when (and (backup-file-name-p file)
+					   (> (- current (float-time (nth 5 (file-attributes file))))
+						  week))
+			  (message "%s" file)
+			  (delete-file file)))
+		  t)
+	  nil)))
 
 (message "END PRUNE-BACKUPS")
