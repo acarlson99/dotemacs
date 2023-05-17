397cd33ff6637167d0718f55263868b6bae0161e
Alexander <bombblob@gmail.com>
2023-05-16 19:56:39 -0700

    style

--- lisp/prune-backups.el
+++ lisp/prune-backups.el
@@ -5,35 +5,39 @@
 
 (message "BEGIN PRUNE-BACKUPS")
 
-;; Make sure backup directory exists
-(if (not (file-directory-p "~/.emacs.d/backups"))
-	(make-directory "~/.emacs.d/backups"))
+(let ((backupdir (mapconcat 'identity `(,user-emacs-directory "backups") ""))
+	  (wipefile (mapconcat 'identity `(,user-emacs-directory ".lastIwiped") "")))
 
-;; Redirects emacs backups
-(setq backup-directory-alist
-	  `((".*" . , "~/.emacs.d/backups")))
-(setq auto-save-file-name-transforms
-	  `((".*" , "~/.emacs.d/backups" t)))
+  ;; Make sure backup directory exists
+  (if (not (file-directory-p backupdir))
+	  (make-directory backupdir))
 
-(let ((last-run-time-file "~/.emacs.d/.lastIwiped")
-	  (week (* 60 60 24 7)))
-  (if (not (file-exists-p last-run-time-file))
-	  (write-region (number-to-string (float-time (current-time))) nil last-run-time-file))
-  (let ((time (with-temp-buffer
-				(insert-file-contents last-run-time-file)
-				(string-to-number (buffer-string))))
-		(current (float-time (current-time))))
-	(if (> current (+ time week))
-		(progn
-		  ;; Purges files not accessed in a week
-		  (message "Deleting old backup files...")
-		  (dolist (file (directory-files "~/.emacs.d/backups" t))
-			(when (and (backup-file-name-p file)
-					   (> (- current (float-time (nth 5 (file-attributes file))))
-						  week))
-			  (message "%s" file)
-			  (delete-file file)))
-		  t)
-	  nil)))
+  ;; Redirects emacs backups
+  (setq backup-directory-alist
+		`((".*" . , backupdir)))
+  (setq auto-save-file-name-transforms
+		`((".*" , backupdir)))
+
+  (let ((last-run-time-file wipefile)
+		(week (* 60 60 24 7)))
+	(if (not (file-exists-p last-run-time-file))
+		(write-region (number-to-string (float-time (current-time))) nil last-run-time-file))
+	(let ((time (with-temp-buffer
+				  (insert-file-contents last-run-time-file)
+				  (string-to-number (buffer-string))))
+		  (current (float-time (current-time))))
+	  (if (> current (+ time week))
+		  (progn
+			;; Purges files not accessed in a week
+			(message "Deleting old backup files...")
+			(dolist (file (directory-files backupdir))
+			  (when (and (backup-file-name-p file)
+						 (> (- current (float-time (nth 5 (file-attributes file))))
+							week))
+				(message "%s" file)
+				(delete-file file)))
+			t)
+		nil)))
+  )
 
 (message "END PRUNE-BACKUPS")
