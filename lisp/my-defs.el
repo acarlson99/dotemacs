7fa954ec41484067c0cf4af4c6a543250d7caa81
Alexander <bombblob@gmail.com>
2023-05-23 22:16:46 -0700

    fix prune-backups

--- lisp/my-defs.el
+++ lisp/my-defs.el
@@ -42,14 +42,13 @@
 (defmacro curry (fun &rest args)
   "Curry curries FUN with ARGS."
   `(lambda (&rest args2)
-    (apply ,fun (append '(,@args) args2))))
+	 (apply ,fun (append '(,@args) args2))))
 
 (defun prune-backups ()
   "Set backup directory to `~/.emacs.d/backups' and clear directory weekly"
   (progn
-	(message "BEGIN PRUNE-BACKUPS")
-	(let ((backupdir (mapconcat 'identity `(,user-emacs-directory "backups") ""))
-		  (wipefile (mapconcat 'identity `(,user-emacs-directory ".lastIwiped") "")))
+	(let ((backupdir (string-join (list user-emacs-directory "backups") ""))
+		  (wipefile (string-join (list user-emacs-directory ".lastIwiped") "")))
 
 	  ;; Make sure backup directory exists
 	  (if (not (file-directory-p backupdir))
@@ -65,23 +64,26 @@
 			(week (* 60 60 24 7)))
 		(if (not (file-exists-p last-run-time-file))
 			(write-region (number-to-string (float-time (current-time))) nil last-run-time-file))
-		(let ((time (with-temp-buffer
-					  (insert-file-contents last-run-time-file)
-					  (string-to-number (buffer-string))))
+		(let ((last-wipe-time (with-temp-buffer
+								(insert-file-contents last-run-time-file)
+								(string-to-number (buffer-string))))
 			  (current (float-time (current-time))))
-		  (if (> current (+ time week))
+		  (if (> current (+ last-wipe-time week))
 			  (progn
 				;; Purges files not accessed in a week
 				(message "Deleting old backup files...")
-				(dolist (file (directory-files backupdir))
+				(dolist (file
+						 (mapcar
+						  (lambda (f) (string-join (list backupdir f) "/"))
+						  (directory-files backupdir)))
 				  (when (and (backup-file-name-p file)
 							 (> (- current (float-time (nth 5 (file-attributes file))))
 								week))
-					(message "%s" file)
+					(message "Delete %s" file)
 					(delete-file file)))
+				(write-region (number-to-string (float-time (current-time))) nil last-run-time-file)
 				t)
-			nil))))
-	(message "END PRUNE-BACKUPS")))
+			(message "No wipe; waiting %d seconds" (- (+ last-wipe-time week) current))))))))
 
 (provide 'my-defs)
 ;;; my-defs.el ends here
