aaf95b3c2814c744439a815a9b4bfe7b2d092e4a
Alexander <bombblob@gmail.com>
2023-10-01 14:22:35 -0700

    setup

---
+++ lisp/web-server-org/setup.el
@@ -0,0 +1,6 @@
+;;; setup.el --- setup deps for web-server-org       -*- lexical-binding: t; -*-
+
+(package-initialize)
+(unless (require 'web-server nil :noerror)
+  (package-install 'web-server)
+  (require 'web-server))
