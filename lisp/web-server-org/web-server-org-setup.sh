c91415ccea6b46b5e677e4954317159f5cd764c3
pi <pi@raspberrypi>
2023-10-14 10:29:07 -0700

    cleanup

--- lisp/web-server-org/web-server-org-setup.sh
+++ lisp/web-server-org/web-server-org-setup.sh
@@ -1,10 +1,17 @@
 # run this to populate files for web-server-org.service
-rm /opt/web-server-org/*.el
-mkdir -p /opt/web-server-org/
+mkdir -p /etc/web-server-org/     # file host dir
+mkdir -p /opt/web-server-org/     # location for elisp files
+rm       /opt/web-server-org/*.el # remove old install if exists
+
+FILES=`cat<<EOF
+https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/web-server-org/web-server-org.el
+https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/web-server-org/setup.el
+https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/el-log.el
+https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/argparse.el
+https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/OwOify.el
+EOF`
 
 cd /opt/web-server-org/ && \
-wget https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/web-server-org/web-server-org.el && \
-    wget https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/web-server-org/setup.el && \
-wget https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/el-log.el && \
-    wget https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/argparse.el && \
-    wget https://raw.githubusercontent.com/acarlson99/dotemacs/master/lisp/OwOify.el
+wget $FILES
+
+emacs --script /opt/web-server-org/setup.el
